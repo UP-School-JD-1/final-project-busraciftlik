090524ce7ef5d3fd38915da37665f220d490f23c
Büşra Çiftlik <busraciftlik96@gmail.com>
2022-10-12 22:05:05 +0300

    driver class refactor

--- src/com/busraciftlik/restaurant/RestaurantSimulationDriver.java
+++ src/com/busraciftlik/restaurant/RestaurantSimulationDriver.java
@@ -5,11 +5,6 @@ import com.busraciftlik.restaurant.model.restaurant.Restaurant;
 
 public class RestaurantSimulationDriver {
     public static void main(String[] args) {
-
-        test();
-    }
-
-    public static void test() {
         Restaurant restaurant = new Restaurant();
         try {
             restaurant.run();
